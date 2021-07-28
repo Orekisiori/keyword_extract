f1e4f0112f8729915af3bbcdfdef7aa667416827
Orekisiori <1612397085@qq.com>
2021-07-29 05:00:36 +0800

    encodingV0.11

--- app/_apis/__init__.py
+++ app/_apis/__init__.py
@@ -1,8 +1,7 @@
 from app.extensions import api
 from .firstApi import FirstApi
-from .rawTest import RawTest
+from .KeywordExtract import KeywordExtract
 
 # 此处注册api
 api.add_resource(FirstApi, '/hello')
-api.add_resource(RawTest,'/kwpu')
 api.add_resource(KeywordExtract,'/kwe')
